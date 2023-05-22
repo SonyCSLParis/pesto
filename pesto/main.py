e571010dc052954d1e5a6b9b7d034f571ca80b03
Martin VANAUD <martin.vanaud@epitech.eu>
2023-05-22 18:13:42 +0200

    fix(pesto packaging) named the main as pesto function to be called from command line

--- pesto/main.py
+++ pesto/main.py
@@ -2,6 +2,6 @@ from pesto.parser import parse_args
 from pesto.predict import predict_from_files
 
 
-if __name__ == "__main__":
+def pesto():
     args = parse_args()
     predict_from_files(**vars(args))

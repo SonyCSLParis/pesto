231db104ccd574fed1cd5f1fc35873c474a00091
Alain Riou <alain.riou14000@yahoo.com>
2024-01-16 23:08:15 +0100

    test gpu

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,8 @@ import os
 
 import pytest
 
+import torch
+
 
 AUDIOS_DIR = os.path.join(os.path.dirname(__file__), "audios")
 
@@ -18,4 +20,20 @@ def test_cli(file, fmt, convert_to_freq):
         suffix = ".semitones." + fmt
         option = " -F"
     os.system(f"pesto {file} --export_format " + fmt + option)
+    out_file = file.rsplit('.', 1)[0] + suffix
+    assert os.path.isfile(out_file)
+    os.unlink(out_file)
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU not available")
+@pytest.mark.parametrize("file, fmt, convert_to_freq",
+                         itertools.product(glob.glob(AUDIOS_DIR + "/*.wav"), ["csv", "npz", "png"], [True, False]))
+def test_cli_gpu(file, fmt, convert_to_freq):
+    if convert_to_freq:
+        suffix = ".f0." + fmt
+        option = ""
+    else:
+        suffix = ".semitones." + fmt
+        option = " -F"
+    os.system(f"pesto {file} --gpu 0 --export_format " + fmt + option)
     assert os.path.isfile(file.rsplit('.', 1)[0] + suffix)
